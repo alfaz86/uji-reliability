faf5b61d6fb230d639a2af63dd819e7a843cd0e5
alfaz86 <hidecode403@gmail.com>
2023-07-15 14:10:51 +0700

    update calculation validity

---
+++ js/validity.js
@@ -0,0 +1,32 @@
+const calculateValidity = () => {
+    var rtabel = selectDataRTable(tingkatSignifikansi, totalRespondent);
+    var valid = '<span class="text-success font-weight-bold">VALID</span>';
+    var notValid = '<span class="text-danger font-weight-bold">TIDAK VALID</span>';
+    var rtabel = selectDataRTable(tingkatSignifikansi, totalRespondent);
+    var rows = ``;
+    var data = [];
+
+    Object.keys(dataQuestions).forEach((key, index) => {
+        var arrXn = dataQuestions[key] != "total" ? dataQuestions[key] : null;
+        var arr$Y = dataQuestions["total"];
+        var R = correl(arrXn, arr$Y);
+        var status = R > rtabel ? valid : notValid;
+
+        if (R > rtabel && dataQuestions[key] != "total") {
+            data.push(dataQuestions[key]);
+        }
+        if (index < totalQuestion) {
+            rows += `<tr>`;
+            rows += `<td>${index + 1}</td>`;
+            rows += `<td>${R}</td>`;
+            rows += `<td>${rtabel}</td>`;
+            rows += `<td>${status}</td>`;
+            rows += `</tr>`;
+        }
+    });
+
+    navVTab.classList.remove("d-none");
+    resultV.innerHTML = rows;
+    validDataQuestions = data;
+    btnR.classList.remove("d-none");
+}
